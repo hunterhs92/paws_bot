ee337b562152e031bfd767db1669faf724f1303c
LHS <hoangson.it.hut@gmail.com>
2024-11-01 01:35:53 +0700

    guide

--- run.bat
+++ run.bat
@@ -30,6 +30,31 @@ if not exist .env (
         echo Đã copy file cấu hình, bỏ qua việc copy.
     )
 
+if not exist data.txt (
+    echo Đang copy file data.txt
+    copy data-example.txt data.txt
+)
+
+if not exist proxy.txt (
+    echo Đang copy file proxy.txt
+    copy proxy-example.txt proxy.txt
+)
+
+if not exist reflink/reflink_10.txt (
+    echo Đang copy file reflink_10.txt
+    copy reflink/reflink_10-example.txt reflink/reflink_10.txt
+)
+
+if not exist reflink/reflink_100.txt (
+    echo Đang copy file reflink_100.txt
+    copy reflink/reflink_100-example.txt reflink/reflink_100.txt
+)
+
+if not exist reflink/reflink_1000.txt (
+    echo Đang copy file reflink_1000.txt
+    copy reflink/reflink_1000-example.txt reflink/reflink_1000.txt
+)
+
 :loop
 python main.py
 echo Đang khởi động lại chương trình sau 30 giây...
