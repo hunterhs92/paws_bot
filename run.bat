13710f30ba0827e296ffbb40f1996e629ebafa72
LHS <hoangson.it.hut@gmail.com>
2024-11-01 10:36:25 +0700

    fix run.bat

--- run.bat
+++ run.bat
@@ -40,19 +40,19 @@ if not exist proxy.txt (
     copy proxy-example.txt proxy.txt
 )
 
-if not exist reflink/reflink_10.txt (
+if not exist reflink\reflink_10.txt (
     echo Đang copy file reflink_10.txt
-    copy reflink/reflink_10-example.txt reflink/reflink_10.txt
+    copy reflink\reflink_10-example.txt reflink\reflink_10.txt
 )
 
-if not exist reflink/reflink_100.txt (
+if not exist reflink\reflink_100.txt (
     echo Đang copy file reflink_100.txt
-    copy reflink/reflink_100-example.txt reflink/reflink_100.txt
+    copy reflink\reflink_100-example.txt reflink\reflink_100.txt
 )
 
-if not exist reflink/reflink_1000.txt (
+if not exist reflink\reflink_1000.txt (
     echo Đang copy file reflink_1000.txt
-    copy reflink/reflink_1000-example.txt reflink/reflink_1000.txt
+    copy reflink\reflink_1000-example.txt reflink\reflink_1000.txt
 )
 
 :loop
